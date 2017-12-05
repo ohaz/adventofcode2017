b4bf16e6fe78522b51f9f5b3e41c1c3c71e031fd
ohaz <thekilleddeath@endless-aerospace.de>
2017-12-05 23:05:59 +0100

    Add solution for day 5

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from day1 import day1
 from day2 import day2
 from day3 import day3
 from day4 import day4
+from day5 import day5
 
 if __name__ == '__main__':
-    days = [day1.calculate_solution, day2.calculate_solution, day3.calculate_solution, day4.calculate_solution]
+    days = [day1.calculate_solution, day2.calculate_solution, day3.calculate_solution, day4.calculate_solution, day5.calculate_solution]
     for index, day in enumerate(days):
         print("Result for day {} is: {}".format(index, day()))
