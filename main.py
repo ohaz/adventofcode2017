f7fecdc141098d5ac75d6c73e4d9aaab80bcd17a
ohaz <thekilleddeath@endless-aerospace.de>
2017-12-09 09:11:58 +0100

    Clean up main file a bit

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from day8 import day8
 from day9 import day9
 
 if __name__ == '__main__':
-    days = [day1.calculate_solution, day2.calculate_solution, day3.calculate_solution, day4.calculate_solution,
-            day5.calculate_solution, day6.calculate_solution, day7.calculate_solution, day8.calculate_solution,
-            day9.calculate_solution]
+    days = [day1, day2, day3, day4, day5, day6, day7, day8, day9]
 
     for index, day in enumerate(days):
-        print("Result for day {} is: {}".format(index+1, day()))
+        print("Result for day {} is: {}".format(index+1, day.calculate_solution()))
